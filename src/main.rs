df6d177656491d2cdea3874748c01b2dd34aa771
David <dcasado@users.noreply.github.com>
2025-03-02 11:06:52 +0100

    add version flag

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+use std::{env, process};
+
 use birthday_sync::{caldav::create_events, carddav::get_contacts, settings::get_settings};
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let flag = &args[1];
+
+    if flag == "--version" {
+        let version = env::var("CARGO_PKG_VERSION").unwrap();
+        println!("{}", version);
+        process::exit(0);
+    }
+
     let settings = get_settings().expect("Failed to get settings");
 
     let contacts = get_contacts(&settings.carddav).expect("Failed to retrieve contacts");
